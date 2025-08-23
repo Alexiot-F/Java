0e1c752777fc9e7d71436fa0f57982fce7d1efcf
Alexiot-F <ajaykapale88@gmail.com>
2025-08-24 00:26:35 +0530

    Character input coder
    Input getted from using System.console() method

---
+++ Char.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Char{
+	
+		public static void main(String args[]) {
+			 String a = System.console().readLine("Enter a word: ");
+				System.out.println(a);
+		}
+	}
